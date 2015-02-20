8dbd23b8283b6687485efd1487c7525a55e7d7bb
Fin-Evan <robertsf@student.unimelb.edu.au>
2015-02-20 17:09:34 +1100

    going to resample WALFA rasters so that they are the same resolution and extent as the 3 areas boundary

--- Fuel-modelling-scripts/TSLF v3.R
+++ Fuel-modelling-scripts/TSLF v3.R
@@ -6,6 +6,23 @@ library(raster)
 library(rgeos)
 library(rgdal)
 
+areas3raster<-raster("/Users/finleyroberts/Documents/spatial_analysis/3 areas new raster/3areas_ras.tif")
+
+
+#resampling to 3 areas raster
+for(i in seq(WALFA_firescars)){
+  r<-raster(WALFA_firescars[i])
+  r[is.na(r)]<-0
+  r[r>1]<-1
+  r<-mask(resample(r,WALFA_boundary_raster, method="ngb"),WALFA_boundary_raster, file=paste0("/Users/finleyroberts/Documents/spatial_analysis/landsat fire scars/Landsat_firescars/new reclassified rasters/3areas",WALFA_firescars[i],"_3areas.tif"),overwrite=TRUE)
+  names(r)<-WALFA_firescars[i]
+  
+  assign(WALFA_firescars[i],r)
+  
+}
+
+
+
 years_chrono<-c("90",
                 "91",
                 "92",
@@ -59,7 +76,7 @@ for(i in seq(years_chrono_numeric)){
   year<-grep(year_str,walfa_rasters, value=TRUE)
   early_year<- year[grepl(paste0("ew",year_str),year,ignore.case=FALSE)]
   rast_year<-raster(early_year)
-  rast_year[rast_year==1]<-years_chrono_numeric[i]+0.4
+  rast_year[rast_year==1]<-years_chrono_numeric[i]+0.6
   writeRaster(rast_year,file=paste0("/Users/finleyroberts/Documents/spatial_analysis/landsat fire scars/Landsat_firescars/num_TSF/",years_chrono_numeric[i],".4_num_tsf.tif"),overwrite=T)
   
 }
@@ -69,58 +86,58 @@ for(i in seq(years_chrono_numeric)){
   year<-grep(year_str,walfa_rasters, value=TRUE)
   late_year<- year[grepl(paste0("lw",year_str),year,ignore.case=FALSE)]
   rast_year<-raster(late_year)
-  rast_year[rast_year==1]<-years_chrono_numeric[i]+0.6
+  rast_year[rast_year==1]<-years_chrono_numeric[i]+0.9
   writeRaster(rast_year,file=paste0("/Users/finleyroberts/Documents/spatial_analysis/landsat fire scars/Landsat_firescars/num_TSF/",years_chrono_numeric[i],".6_num_tsf.tif"),overwrite=T)
   
 }
 
 
-season_reverse_chrono<-c(2011.6,
-                         2011.4,
+season_reverse_chrono<-c(2011.9,
+                         2011.6,
+                         2010.9,
                          2010.6,
-                         2010.4,
+                         2009.9,
                          2009.6,
-                         2009.4,
+                         2008.9,
                          2008.6,
-                         2008.4,
+                         2007.9,
                          2007.6,
-                         2007.4,
+                         2006.9,
                          2006.6,
-                         2006.4,
+                         2005.9,
                          2005.6,
-                         2005.4,
+                         2004.9,
                          2004.6,
-                         2004.4,
+                         2003.9,
                          2003.6,
-                         2003.4,
+                         2002.9,
                          2002.6,
-                         2002.4,
+                         2001.9,
                          2001.6,
-                         2001.4,
+                         2000.9,
                          2000.6,
-                         2000.4,
+                         1999.9,
                          1999.6,
-                         1999.4,
+                         1998.9,
                          1998.6,
-                         1998.4,
+                         1997.9,
                          1997.6,
-                         1997.4,
+                         1996.9,
                          1996.6,
-                         1996.4,
+                         1995.9,
                          1995.6,
-                         1995.4,
+                         1994.9,
                          1994.6,
-                         1994.4,
+                         1993.9,
                          1993.6,
-                         1993.4,
+                         1992.9,
                          1992.6,
-                         1992.4,
+                         1991.9,
                          1991.6,
-                         1991.4,
+                         1990.9,
                          1990.6,
-                         1990.4,
-                         1989.6,
-                         1989.4)
+                         1989.9,
+                         1989.6)
 
 
 
